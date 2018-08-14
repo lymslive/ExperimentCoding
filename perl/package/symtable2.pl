fec1932c0867e44e518b8a018503f4637eee4a9c
lymslive <403708621@qq.com>
2018-08-15 00:02:20 +0800

    get symbol by module name

--- perl/package/symtable2.pl
+++ perl/package/symtable2.pl
@@ -3,13 +3,28 @@ package symtable;
 use strict;
 use warnings;
 
-# use File::Spec;
-use File::Find;
-print "symbols in package: File::Find\n";
-foreach my $key (sort keys %File::Find::) {
-	print "$key\n";
+main(@ARGV) unless caller;
+
+sub main_default
+{
+	use File::Find;
+	print "symbols in package: File::Find\n";
+	foreach my $key (sort keys %File::Find::) {
+		print "$key\n";
+	}
+	# GetSymbols(\%{File::Find::});
 }
-GetSymbols(\%{File::Find::});
+
+sub main
+{
+	my ($name) = @_;
+	if ($name) {
+		GetSymByName($name);
+	}
+	else {
+		main_default();
+	}
+}
 
 sub GetSymbols
 {
@@ -19,6 +34,13 @@ sub GetSymbols
 	foreach my $key (sort keys %$pack_ref) {
 		print "$key\n";
 	}
+}
+
+sub GetSymByName
+{
+	my ($name) = @_;
+	my $pack_ref = eval('\%' . $name . '::');
+	GetSymbols($pack_ref, $name);
 }
 
 # 面向对象风格的模块，打印的符号表较少，面向过程的模块，符号表比较多
