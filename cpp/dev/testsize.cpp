548793fd067f4b0e8c08c78696d35dbbc7fe94dd
lymslive <403708621@qq.com>
2018-11-25 23:29:08 +0800

    new exp

--- cpp/dev/testsize.cpp
+++ cpp/dev/testsize.cpp
@@ -15,5 +15,18 @@ int main(int argc, char *argv[])
 	strcpy(hstr.stack_+1, "abcdefg");
 	printf("withhead: %s[%d]\n", hstr.stack_, strlen(hstr.stack_));
 	printf("withouthead: %s[%d]\n", hstr.stack_+1, strlen(hstr.stack_+1));
+
+	printf("sizeof(SStrRep) = %d\n", sizeof(SStrRep));
+	printf("sizeof(SStrRep1) = %d\n", sizeof(SStrRep1));
+	printf("sizeof(SStrRep2) = %d\n", sizeof(SStrRep2));
+
+	CScalar sc;
+	printf("sizeof(CScalar) = %d\n", sizeof(CScalar));
+	printf("sizeof(sc) = %d\n", sizeof(sc));
+	printf("sizeof(sc.sbig_) = %d\n", sizeof(sc.sbig_));
+	printf("sizeof(sc.small_) = %d\n", sizeof(sc.small_));
+	printf("sizeof(sc.sdata_) = %d\n", sizeof(sc.sdata_));
+	printf("sizeof(sc.sbig_.val) = %d\n", sizeof(sc.sbig_.val));
+	printf("sizeof(sc.small_.val) = %d\n", sizeof(sc.small_.val));
 	return 0;
 }
--- cpp/dev/testsize.h
+++ cpp/dev/testsize.h
@@ -2,6 +2,7 @@
 #define TESTSIZE_H__
 
 #include <stdint.h>
+#include <stddef.h>
 
 struct HStrRep
 {
@@ -22,4 +23,61 @@ public:
 	};
 };
 
+struct SStrRep
+{
+	size_t len;
+	char* str;
+};
+
+struct SStrRep1
+{
+	int len;
+	char* str;
+};
+
+struct SStrRep2
+{
+	char* str;
+	int len;
+};
+
+class CScalar
+{
+public:
+	union UValue
+	{
+		bool bValue;
+		int iValue;
+		long lValue;
+		float fValue;
+		double dValue;
+		uint64_t uValue;
+	};
+
+	struct ScalarRep 
+	{
+		uint8_t head;
+		uint8_t head1;
+		uint8_t head2;
+		uint8_t head3;
+		uint32_t refcount;
+		uint32_t capcity;
+		uint32_t length;
+		char* ptr;
+		UValue val;
+	};
+
+	struct SmallRep
+	{
+		uint8_t head;
+		char data[32-1-8];
+		UValue val;
+	};
+
+	union {
+		ScalarRep sbig_;
+		SmallRep small_;
+		char sdata_[32];
+	};
+};
 #endif /* end of include guard: TESTSIZE_H__ */
